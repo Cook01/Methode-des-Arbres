fa3c09c31342fb54ed8324111544b3b4dbdf456b
gkueny <gaetankueny@gmail.com>
2016-11-21 16:13:22 +0100

    update con troller

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -7,10 +7,20 @@ class MainController
 	public static void main(String[] args)
 	{
 
-		Lettre a = new Lettre("a", false);
-        Lettre b = new Lettre("b", false);
+		
+        Lettre a 	= new Lettre("a", false);
+        Lettre b 	= new Lettre("b", false);
+        Lettre d 	= new Lettre("d", false);
+        Lettre cNeg = new Lettre("c", true);
 
-        SousFormule formule = new SousFormule(a, Symbole.IMPLIQUE, b, false);
+
+        SousFormule formule1 	= new SousFormule(a, Symbole.IMPLIQUE, b, false);//(a -> b)
+
+        SousFormule formule2 	= new SousFormule(formule1, Symbole.ET, cNeg, false);//((a -> b) && -(c))
+
+        SousFormule formule3 	= new SousFormule(d, Symbole.OU, formule2, true);//-(d || ((a -> b) && -(c)))
+
+        SousFormule formule 	= new SousFormule(formule2, Symbole.ET, formule3, false);//(((a -> b) && -(c)) && -(d || ((a -> b) && -(c))))
 
         System.out.println(formule);
 
@@ -29,7 +39,7 @@ class MainController
         	System.out.println(formule.getFormuleA());
         	System.out.println(formule.getFormuleB());
         } else {
-        	System.out.println(formule.getFormuleA() + "    	    	" + formule.getFormuleB());
+        	System.out.println(formule.getFormuleA() + "	" + formule.getFormuleB());
         }
 
 	}
