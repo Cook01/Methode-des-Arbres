784be31d9766289b6e64f9ea82d5fd2128e1bf6f
Cook <vincentthomasetude@gmail.com>
2016-11-21 16:12:52 +0100

    Identation du main

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -8,29 +8,28 @@ class MainController
 	{
 
 		Lettre a = new Lettre("a", false);
-        Lettre b = new Lettre("b", false);
+                Lettre b = new Lettre("b", false);
 
-        SousFormule formule = new SousFormule(a, Symbole.IMPLIQUE, b, false);
+                SousFormule formule = new SousFormule(a, Symbole.IMPLIQUE, b, false);
 
-        System.out.println(formule);
+                System.out.println(formule);
 
 
-        Decomposition decomposeFormule = new Decomposition(formule);
+                Decomposition decomposeFormule = new Decomposition(formule);
 
-        if (decomposeFormule.getAisNeg()) {
-        	formule.getFormuleA().setIsNeg();
-        }
+                if (decomposeFormule.getAisNeg()) {
+                	formule.getFormuleA().setIsNeg();
+                }
 
-        if (decomposeFormule.getBisNeg()) {
-        	formule.getFormuleB().setIsNeg();
-        }
+                if (decomposeFormule.getBisNeg()) {
+                	formule.getFormuleB().setIsNeg();
+                }
 
-        if (decomposeFormule.getAonNeg()) {
-        	System.out.println(formule.getFormuleA());
-        	System.out.println(formule.getFormuleB());
-        } else {
-        	System.out.println(formule.getFormuleA() + "    	    	" + formule.getFormuleB());
-        }
-
+                if (decomposeFormule.getAonNeg()) {
+                	System.out.println(formule.getFormuleA());
+                	System.out.println(formule.getFormuleB());
+                } else {
+                	System.out.println(formule.getFormuleA() + "    	    	" + formule.getFormuleB());
+                }
 	}
 }
